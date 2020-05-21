f350362009ea687356ea358e06b588db45b4c29f
David Reisenauer <David@Davids-MacBook-Pro-3.local>
2020-05-21 21:21:05 +0200

    leaflet elevation test

--- main_s2.js
+++ main_s2.js
@@ -10,5 +10,52 @@ let map = L.map("map", {
     ]
 });
 
+let themenradwege = " https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:THEMENRADWEGOGD&srsName=EPSG:4326&outputFormat=json"
 
 //Funktionen
+
+L.geoJson.ajax(themenradwege, {
+    style: function (feature) {
+        if (feature.properties.BEZEICHNUNG === "Wiental-Radweg") {
+            return {
+                color: "red"
+            };
+        }if (feature.properties.BEZEICHNUNG === "Ring-Rund-Radweg") {
+            return {
+                color: "green"
+            };
+        }if (feature.properties.BEZEICHNUNG === "Gürtel-Radweg") {
+            return {
+                color: "yellow"
+            };
+
+        }if (feature.properties.BEZEICHNUNG === "Donau-Radweg") {
+            return {
+                color: "purple"
+            };
+
+        }if (feature.properties.BEZEICHNUNG === "City-Radweg") {
+            return {
+                color: "black"
+            };
+        }if (feature.properties.BEZEICHNUNG === "Bernstein-Radweg") {
+            return {
+                color: "pink"
+            }
+        }
+    },
+    onEachFeature: function (feature) {
+        console.log(feature);
+    }
+}).addTo(map);
+
+let test = themenradwege.feature.properties.BEZEICHNUNG === "Wiental-Radweg";
+
+let controlElevation = L.control.elevation({
+    theme: "adler-theme",
+    detached: true,
+    elevationDiv: "#profile",
+    followMarker: false
+}).addTo(map);
+
+controlElevation.load(test)
