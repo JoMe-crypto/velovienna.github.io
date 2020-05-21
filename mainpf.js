d026406de747e5513a3563c97f756aa4eccc2d45
Pia Feiel <pia@Pias-Air.fritz.box>
2020-05-21 19:14:58 +0200

    Grundgerüst Seite 1 erstellt

---
+++ mainpf.js
@@ -0,0 +1,8 @@
+let map = document.querySelector("#map")
+
+var mymap = L.map(map).setView([48.208354, 16.372504], 13)
+
+L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+    maxZoom: 17,
+    attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>tributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https:/ntopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+}).addTo(mymap);
