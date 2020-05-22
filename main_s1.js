613a4f708e7be7ae5bd649417fadab5617f433f4
Pia Feiel <pia@Pias-Air.fritz.box>
2020-05-22 15:27:07 +0200

    Hintergrundkarten ändern

--- main_s1.js
+++ main_s1.js
@@ -1,8 +1,27 @@
-let map = document.querySelector("#map")
+let startLayer = L.tileLayer.provider("BasemapAT");
 
-var mymap = L.map(map).setView([48.208354, 16.372504], 13)
+let map = L.map("map", {
+    center: [48.208333, 16.373056],
+    zoom: 12,
+    layers: [
+        startLayer
+    ]
+});
 
-L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
-    maxZoom: 17,
-    attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>tributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https:/ntopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
-}).addTo(mymap);
+// var mymap = L.map(map).setView([48.208354, 16.372504], 13)
+
+// L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+//     maxZoom: 17,
+//     attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>tributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https:/ntopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+// }).addTo(mymap);
+
+L.control.layers({
+    "BasemapAT": startLayer,
+    "CycleOSM": L.tileLayer.provider("CyclOSM"),
+    // "BasemapAT.terrain": L.tileLayer.provider("BasemapAT.terrain"),
+    // "BasemapAT.surface": L.tileLayer.provider("BasemapAT.surface"),
+    "BasemapAT.orthofoto": L.tileLayer.provider("BasemapAT.orthofoto"),
+    "Stamen.Terrain": L.tileLayer.provider("Stamen.Terrain"),
+    "OpenTopoMap": L.tileLayer.provider("OpenTopoMap")
+}).addTo(map);
+
