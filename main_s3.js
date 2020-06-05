55357e1b5416d595b164beafecceb330c5a49c02
JoMe-crypto <johannes.melchert@student.uibk.ac.at>
2020-06-05 19:00:30 +0200

    zwischen durch sichern - Karte nicht angezeigt

--- main_s3.js
+++ main_s3.js
@@ -1,8 +1,12 @@
 let startLayer = L.titleLayer.provider("BasemapAT");
-let map = L.map("map",{
+let map = L.map("map", {
     center: [48.20833, 16.373056],
     zoom: 12, 
     layers:[
         startLayer
     ]
 });
+
+L.control.layers({
+    "BasemapAT": startLayer
+}).addTo(map);
